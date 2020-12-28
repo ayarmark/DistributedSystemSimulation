b742c64fe601f077c592e35f9d951412e49a90bc
ayarmark <58202564+ayarmark@users.noreply.github.com>
2020-12-28 00:32:01 -0500

    read job and slave classes

---
+++ src/Slave.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import client.Job;
+
+public class Slave {
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		args = new String[] { "30121" };
+
+		if (args.length != 1) {
+			System.err.println("Usage: java EchoServer <port number>");
+			System.exit(1);
+		}
+
+		int portNumber = Integer.parseInt(args[0]);
+
+		ReadJob readJob = new ReadJob(portNumber);
+		readJob.run();
+		
+		// System.out.println(portNumber);
+		// Socket clientSocket = new Socket("127.0.0.1", 30121);
+		Socket socket = new Socket();
+		System.out.println(socket.getInetAddress());
+
+		ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+		try {
+			Job returnedJob = (Job) in.readObject();
+			System.out.println(returnedJob.getJobType());
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+		socket.close();
+	}
+}
