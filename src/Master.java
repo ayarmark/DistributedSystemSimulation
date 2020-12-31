a43bd49d0442e7e98cea45ec3053fa603d99fb06
pzieg <pziegler@student.touro.edu>
2020-12-30 20:07:39 -0500

    Penina client pass to master

--- src/Master.java
+++ src/Master.java
@@ -1,19 +1,39 @@
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.*;
 
 import client.Job;
 import client.JobType;
+import write.WriteJob;
 
-
+/**///added import write job
 public class Master 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws UnknownHostException, IOException
 	{
+		
+		/**///start here
+		Socket serverSocket = new Socket("127.0.0.1", 14725);
+		ObjectInputStream in = new ObjectInputStream(serverSocket.getInputStream());
+	     /**///ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+	    try {
+			Job returnedJob = (Job)in.readObject();
+			System.out.println(returnedJob.getJobType());
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	     /**///end here
+		
 		Job job = new Job(JobType.A, 1);
 		//Socket socket = new Socket();
 		WriteJob write  = new WriteJob("127.0.0.1", 123);
 		write.start();
 		write.run();
 		write.sendJob(job);
+		
+		
+		
 	}	
 }
--- src/client/Client.java
+++ src/client/Client.java
@@ -1,10 +1,50 @@
 package client;
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+import write.WriteJob;
 public class Client 
 {
 	public static void main(String [] args) throws UnknownHostException, IOException
 	{
+		
+	     /**///start here
+	     /**///moved writejob to new package
+	     int clientID = 10;//need to set
+	     
+	     //System.out.print("Enter A or B for the type of job you want to perform: ");
+	     //BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+	     //String userInput;
+	     JobType jobType;
+	     Random rand = new Random();
+	     WriteJob write  = new WriteJob("127.0.0.1", 14725);
+	     write.start();
+	     write.run();
+	     
+	     //while ((userInput = stdIn.readLine()) != null) {
+	     for(int i=0; i<30; i++) {
+	    	 //if(userInput.toUpperCase() == "A" || userInput.toUpperCase() == "B") {
+	    	 	if(rand.nextInt(2) == 1) {
+	    	 		jobType = JobType.A;
+	    	 	}
+	    	 	else {
+	    	 		jobType = JobType.B;
+	    	 	}
+	    		//jobType = JobType.valueOf(userInput);
+	    		System.out.println("Sending request for job type " + jobType + "to server...");
+	    		Job job = new Job(jobType, clientID);
+	    	    write.sendJob(job);
+	    	 //}
+	    	 //else {
+	    	 //	 System.out.println("That is not a correct choice.");
+	    	 //}             
+             //System.out.print("Enter A or B for the next type of job you want to perform: ");
+         }
+	     
+	     /**///end here
+		
 		args = new String[] {"127.0.0.1", "30121"};
     	
         if (args.length != 2) {
@@ -18,9 +58,10 @@ public class Client
 	     //System.out.println(portNumber);
 	     Socket clientSocket = new Socket(hostName, portNumber);
 
+
 	     
 	     ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
-	     ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+	     /**///ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
 	     try {
 			Job returnedJob = (Job)in.readObject();
 			System.out.println(returnedJob.getJobType());
@@ -28,5 +69,7 @@ public class Client
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	     
+
 	}
 }
